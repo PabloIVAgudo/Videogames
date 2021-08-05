c391fee9305d80243ee293d2a33e5aeb7d6fe4a3
PabloIVAgudo <agudopablo@gmail.com>
2021-08-05 16:26:59 -0300

    PI in progress

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders "Henry Videogames" ', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("Henry Videogames");
   expect(linkElement).toBeInTheDocument();
 });
